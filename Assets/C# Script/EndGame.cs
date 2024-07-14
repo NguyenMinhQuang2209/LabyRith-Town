e8c86f0039c336798ea29b45eaf3d874d92c166a
Quang Minh <quangnm2209@gmail.com>
2024-07-14 22:49:14 +0700

    adding endgame

---
+++ Assets/C# Script/EndGame.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class EndGame : MonoBehaviour
+{
+    public Transform player;
+    public Animator playerAnimator;
+    public float MoveSpeed = 1f;
+    public Transform targetPos;
+    public TextMeshProUGUI childTxt;
+    public GameObject endGame;
+    private void Start()
+    {
+        endGame.SetActive(false);
+    }
+    private void Update()
+    {
+        if (player.transform.position.x >= targetPos.position.x)
+        {
+            playerAnimator.enabled = false;
+            endGame.SetActive(true);
+        }
+        else
+        {
+            player.transform.position = new(player.transform.position.x + MoveSpeed * Time.deltaTime, player.transform.position.y, player.transform.position.z);
+        }
+    }
+}
